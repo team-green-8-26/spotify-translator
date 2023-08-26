ec2660d5bb6e351f3a246a4e286b402971ad2702
idrios <jimroberts10@gmail.com>
2023-08-26 13:47:29 -0400

    added sample get request for a song

--- src/api.py
+++ src/api.py
@@ -1,11 +1,28 @@
 import os
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from pydantic import BaseModel
 import socket
 import uvicorn
+from typing import Dict
 
 app = FastAPI()
 port = 6222
+
+
+class MediaInfo(BaseModel):
+    id: int
+    artist: str
+    songname: str
+    filename: str
+    media_type: str
+
+
+"""
+Useful fastapi info: 
+ - Can see api info by going to '/docs' endpoint
+ - 
+"""
 
 
 @app.get("/")
@@ -13,6 +30,14 @@ def index() -> FileResponse:
     webdir = os.path.dirname(__file__)
     index = os.path.join(webdir, '../web/index.html')
     return FileResponse(index)
+
+
+@app.get("/song")
+def index(artist: str, songname: str) -> Dict[str, str]:
+    return {
+        "items": f"{songname} by {artist}",
+        "description": "In the future this will send an actual song"
+    }
 
 
 def start_api():
