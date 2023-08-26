73b29272bc6d67562d75667f9d2b406cb80be073
Toilaf <enby.adams@gmail.com>
2023-08-26 18:48:41 -0400

    Simplify lyrics

--- frontend/src/components/BodyContainer.jsx
+++ frontend/src/components/BodyContainer.jsx
@@ -2,25 +2,17 @@ import React, {useState, useEffect} from 'react';
 import '../assets/css/body-container.css';
 import LyricsContainer from './LyricsContainer';
 import FlagContainer from './FlagContainer';
-import axios from "axios";
+
+import originalLyrics from "./database/nainowale-ne.txt"
 
 const lyrics = [
-    [1.01, "one", 1],
-    [2.20, "two", 1],
-    [3.31, "three", 2]
+    ["one", 1],
+    ["two", 1],
+    ["three", 2]
 ]
 
 function BodyContainer(){
     const [selectedLyric, setSelectedLyric] = useState(null);
-    const [englishLyrics, setEnglishLyrics] = useState({});
-    const [originalLyrics, setOriginalLyrics] = useState({});
-    useEffect(() => {
-        let englishLyrics, originalLyrics;
-        async function fetchLyrics(){
-            englishLyrics = await axios.get('')
-        }
-        fetchLyrics();
-    }, []);
     const updateShowFlag = (lyric) => {
         setSelectedLyric(lyric);
     }
--- frontend/src/components/LyricsContainer.jsx
+++ frontend/src/components/LyricsContainer.jsx
@@ -5,12 +5,12 @@ function LyricsContainer(props){
     const [selectedSentenceNumber, setSelectedSentenceNumber] = useState(false);
 
     const createLyric = (lyric) => {
-        return <li onClick={() => handleLyricClick(lyric)} className={(selectedSentenceNumber == lyric[2])?'highlighted':''}>{lyric[0]} {lyric[1]}</li>
+        return <li onClick={() => handleLyricClick(lyric)} className={(selectedSentenceNumber == lyric[1])?'highlighted':''}>{lyric[0]}</li>
     }
 
     const handleLyricClick = (lyric) => {
         if (props.updateShowFlag) {
-            setSelectedSentenceNumber(lyric[2]);
+            setSelectedSentenceNumber(lyric[1]);
             props.updateShowFlag(lyric);
         }
     }
