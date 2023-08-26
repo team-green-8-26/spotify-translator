d7d1947b1a4514373c89633a420021c8d5ead3f1
idrios <jimroberts10@gmail.com>
2023-08-26 14:33:27 -0400

    basic structure of API

--- src/api.py
+++ src/api.py
@@ -12,28 +12,51 @@ port = 6222
 
 class MediaInfo(BaseModel):
     id: int
-    artist: str
-    songname: str
-    filename: str
-    media_type: str
 
 
 """
-Useful fastapi info: 
+FastAPI on a uvicorn server:
+ - Go to https://fastapi.tiangolo.com/tutorial/ 
+ - Or follow this tutorial https://www.youtube.com/watch?v=SORiTsvnU28
  - Can see api info by going to '/docs' endpoint
- - 
 """
 
 
 @app.get("/")
 def index() -> FileResponse:
+    """
+    Summary of the API
+    """
     webdir = os.path.dirname(__file__)
     index = os.path.join(webdir, '../web/index.html')
     return FileResponse(index)
 
 
 @app.get("/song")
+def index() -> FileResponse:
+    """
+    Return all hardcoded songs in the database
+    """
+    webdir = os.path.dirname(__file__)
+    index = os.path.join(webdir, '../web/index.html')
+    return FileResponse(index)
+
+
+@app.get("/song/{id}")
+def index() -> FileResponse:
+    """
+    Return song of specific id
+    """
+    webdir = os.path.dirname(__file__)
+    index = os.path.join(webdir, '../web/index.html')
+    return FileResponse(index)
+
+
+@app.post("/report/{id}")
 def index(artist: str, songname: str) -> Dict[str, str]:
+    """
+    Report a bad translation
+    """
     return {
         "items": f"{songname} by {artist}",
         "description": "In the future this will send an actual song"
---
+++ src/appmanager.py
@@ -0,0 +1,2 @@
+# god-object that connects all parts of the app together
+
