dd55a5b41e3f7f1bb54420dd61f3c36072044652
Toilaf <enby.adams@gmail.com>
2023-08-26 12:40:01 -0400

    Add sample interactions to test API

--- spotify-translator/src/App.js
+++ spotify-translator/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import SampleAPIInteractions from './components/SampleAPIInteractions';
 
 function App() {
   return (
     <div className="App">
       <h1>Spotify Translator</h1>
+      <SampleAPIInteractions/>
     </div>
   );
 }
---
+++ spotify-translator/src/components/SampleAPIInteractions.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function SampleAPIInteractions() {
+    return <ls>
+        <button>Thumbs Up</button>
+        <button>Thumbs Down</button>
+        <textarea>Paste In Lyrics</textarea>
+        <button>Translate Lyrics</button>
+        <textbox>Name of Song</textbox>
+        <button>Get Audio</button>
+    </ls>
+}
