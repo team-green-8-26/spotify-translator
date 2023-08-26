2dd40792235a50865dc8094645dbeb4e5473b6dc
Toilaf <enby.adams@gmail.com>
2023-08-26 17:42:44 -0400

    Change goofy image

--- frontend/src/components/FooterContainer.jsx
+++ frontend/src/components/FooterContainer.jsx
@@ -1,12 +1,12 @@
 import React, {useState, useEffect} from 'react';
 import '../assets/css/footer-container.css';
 
-const raccoon = "https://designshack.net/wp-content/uploads/passion-raccoon.jpg"
+const goofyimg = "https://designshack.net/wp-content/uploads/passion-frog2.jpg"
 
 function FooterContainer(){
     return (
         <div className='footer-container'>
-            <img src={raccoon} alt="Image of a raccoon on an ocean background labeled 'Graphic design is my passion'"/>
+            <img src={goofyimg} alt="Image of a frog on a backdrop of a blue sky labeled 'Graphic design is my passion'"/>
         </div>
     )
 }
