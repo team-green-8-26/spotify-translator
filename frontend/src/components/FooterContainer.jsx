911826b411359c66f76ff2114fefd7da6b66b925
Toilaf <enby.adams@gmail.com>
2023-08-26 16:41:22 -0400

    Add goofy raccoon

--- frontend/src/components/FooterContainer.jsx
+++ frontend/src/components/FooterContainer.jsx
@@ -1,10 +1,12 @@
 import React, {useState, useEffect} from 'react';
 import '../assets/css/footer-container.css';
 
+const raccoon = "https://designshack.net/wp-content/uploads/passion-raccoon.jpg"
+
 function FooterContainer(){
     return (
         <div className='footer-container'>
-            <h1>Spotify Translator</h1>
+            <img src={raccoon} alt="Image of a raccoon on an ocean background labeled 'Graphic design is my passion'"/>
         </div>
     )
 }
