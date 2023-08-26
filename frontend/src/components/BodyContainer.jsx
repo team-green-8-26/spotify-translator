681e8b2ebfa55c821b04956c8d0219c63008a86e
sushrutm29 <smadhavi@stevens.edu>
2023-08-26 18:08:15 -0400

    some updates

--- frontend/src/components/BodyContainer.jsx
+++ frontend/src/components/BodyContainer.jsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import '../assets/css/body-container.css';
 import LyricsContainer from './LyricsContainer';
 import FlagContainer from './FlagContainer';
+import axios from "axios";
 
 const lyrics = [
     [1.01, "one", 1],
@@ -11,6 +12,15 @@ const lyrics = [
 
 function BodyContainer(){
     const [selectedLyric, setSelectedLyric] = useState(null);
+    const [englishLyrics, setEnglishLyrics] = useState({});
+    const [originalLyrics, setOriginalLyrics] = useState({});
+    useEffect(() => {
+        let englishLyrics, originalLyrics;
+        async function fetchLyrics(){
+            englishLyrics = await axios.get('')
+        }
+        fetchLyrics();
+    }, []);
     const updateShowFlag = (lyric) => {
         setSelectedLyric(lyric);
     }
